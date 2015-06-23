650331359f2a40c07a3f4bc37487f11df08b0cc8
Ronan Mc Guire <ronanir600@gmail.com>
2015-06-23 12:26:43 -0600

    updated validator

--- main.js
+++ main.js
@@ -7,31 +7,41 @@ var marriageSmallCaps = getMarriage.toLowerCase();
 
 
 
-if((getBirth.charAt(2) === '/' && getBirth.charAt(5) === '/') && (parseInt(getBirth.charAt(0)) && parseInt(getBirth.charAt(1)) && parseInt(getBirth.charAt(3)) && parseInt(getBirth.charAt(4)) && parseInt(getBirth.charAt(6)) && parseInt(getBirth.charAt(7)) ) && (getBirth.length === 8) ){
-	alert(true);
-		} else {
+if((getBirth.charAt(2) === '/' && getBirth.charAt(5) === '/') &&
+(parseInt(getBirth.charAt(0)) && parseInt(getBirth.charAt(1)) && 
+	parseInt(getBirth.charAt(3)) && parseInt(getBirth.charAt(4)) 
+	&& parseInt(getBirth.charAt(6)) && parseInt(getBirth.charAt(7)) ) 
+	&& (getBirth.length === 8) ){
+			alert(true);
+				} else {
 			alert(false);
-		}
+				}
 
-if ( (getAddress.length === 5 && parseInt(getAddress.charAt(0)) && parseInt(getAddress.charAt(1)) && parseInt(getAddress.charAt(2)) && parseInt(getAddress.charAt(3)) && parseInt(getAddress.charAt(4)) ) || (getAddress.length === 10 && getAddress.charAt(5) === '-' && parseInt(getAddress.charAt(0)) && parseInt(getAddress.charAt(1)) && parseInt(getAddress.charAt(2)) && parseInt(getAddress.charAt(3)) && parseInt(getAddress.charAt(4)) && parseInt(getAddress.charAt(6)) && parseInt(getAddress.charAt(7)) && parseInt(getAddress.charAt(8)) && parseInt(getAddress.charAt(9))) ) {
-	alert(true);
-			} else {
-				alert(false);
-			}
+if ( (getAddress.length === 5 && parseInt(getAddress.charAt(0)) && 
+	parseInt(getAddress.charAt(1)) && parseInt(getAddress.charAt(2)) 
+	&& parseInt(getAddress.charAt(3)) && parseInt(getAddress.charAt(4)) ) 
+	|| (getAddress.length === 10 && getAddress.charAt(5) === '-' && parseInt(getAddress.charAt(0)) 
+		&& parseInt(getAddress.charAt(1)) && parseInt(getAddress.charAt(2)) && parseInt(getAddress.charAt(3)) 
+		&& parseInt(getAddress.charAt(4)) && parseInt(getAddress.charAt(6)) && parseInt(getAddress.charAt(7)) 
+		&& parseInt(getAddress.charAt(8)) && parseInt(getAddress.charAt(9))) ) {
+			alert(true);
+				} else {
+			alert(false);
+					}
 
 
-if ( (getAbv.length === 2) && (isNaN(getAbv.charAt(0)) ) &&  (isNaN(getAbv.charAt(1)) ) && (getAbv === getAbv.toUpperCase() ) ) {
-	alert(true);
-			} else {
-				alert(false);
-			}
+if ( (getAbv.length === 2) && (isNaN(getAbv.charAt(0)) ) &&  
+	(isNaN(getAbv.charAt(1)) ) && (getAbv === getAbv.toUpperCase() ) ) {
+			alert(true);
+				} else {
+			alert(false);
+				}
 
 
 if ((marriageSmallCaps === 'yes') || (marriageSmallCaps === 'no')) {
-
-	alert(true);
-		} else {
+			alert(true);
+				} else {
 			alert(false);
-		}
+			}
 
 
